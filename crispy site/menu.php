4cc69598835f1f71e61890dde8453dae6b3aba22
bilalfatihbayrak <90700640+bilalfatihbayrak@users.noreply.github.com>
2023-03-24 15:23:33 +0300

    Add files via upload

---
+++ crispy site/menu.php
@@ -0,0 +1,189 @@
+
+<html>
+
+<head>
+<title>Crispy Kitchen</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    
+<nav class="navbar navbar-expand-sm bg-light justify-content-center">
+<div style="padding-right:10%;">
+    <h3>Crispy Kitchen</h3>
+    </div>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Story</a>
+    </li>
+    <li class="nav-item">
+      <a style="color:red;" class="nav-link" href="#">Menu</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Our Updates</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Contact</a>
+    </li>
+  </ul>
+  <div style="padding-left:10%;">
+    <button type="button" class="btn btn-danger">Rezervation</button>
+    </div>
+</nav>
+
+<div class="container-fluid mt-3">
+  
+</div>
+
+<div class="container">
+<img style="width:103%; height:70%;" src="img/luisa-brimble-aFzg83dvnAI-unsplash.jpg">
+<div class="centered"><h1>Our Menus</h1><h5>Perfect for ill Breakfast Lunch and Dinner</h5></div>
+</div>
+
+    <div class="row margin-align">   
+        <h2 style="text-align:center; margin-bottom:5%;">Breakfast Menu</h2>
+        <div class="col-sm-4">
+            <img src="img/brett-jordan-8xt8-HIFqc8-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Fresh Start</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:22%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img src="img/lucas-swennen-1W_MyJSRLuQ-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Baked Creamy</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:22%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img src="img/louis-hansel-dphM2U1xq0U-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Burger Set</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:22%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row margin-align">   
+        <h2 style="text-align:center; margin-bottom:5%;">Lunch Menu</h2>
+        <div class="col-sm-6">
+            <img src="img/louis-hansel-cH5IPjaAYyo-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Super Steak Set</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:15%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <img src="img/louis-hansel-rheOvfxOlOA-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Bread And Steak Set</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:15%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+         </div>
+        </div>
+
+
+        <div class="row margin-align">   
+        <h2 style="text-align:center; margin-bottom:5%;">Dinner Menu</h2>
+        <div class="col-sm-4">
+            <img src="img/farhad-ibrahimzade-ZipYER3NLhY-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Sea Food Set</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:22%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img src="img/keriliwi-c3mFafsFz2w-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Premium Steak</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:22%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <img src="img/farhad-ibrahimzade-isHUj3N0194-unsplash.jpg" alt="" width=100%>
+            <h5 style="padding-left:10%;">Salmon Set</h5>
+            <div class="row">
+            <div class="col-sm-6" style="padding:2%; border-radius:40%; background-color:#EFEFEF; width:22%; color:orange; margin-left:9%;"><h5>$24.50</h5></div>
+            <div class="col-sm-6">
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star checked"></span>
+             <span class="fa fa-star"></span>
+             <span class="fa fa-star"></span>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+<img style="width:103%; height:70%;" src="img/daan-evers-tKN1WXrzQ3s-unsplash.jpg">
+<div class="centered"><h1 style="text-align:left;">Crispy Kitchen</h1></div>
+</div>
+
+</body>
+
+</html>
